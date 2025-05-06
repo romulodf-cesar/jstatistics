955916d8d3e111103fe87ce4126144bc31daddf6
romulodf-cesar <romulodf.cesar@gmail.com>
2025-05-06 11:45:00 -0300

    aula amanhã

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô Git!");
+        System.out.println("Sozinhos\r\n" + //
+                        "Faça como eu fiz\r\n" + //
+                        "\r\n" + //
+                        "Criar um jogo de lançar dados similar ao jogo do cara ou cora.\r\n" + //
+                        "=============================================================\r\n" + //
+                        "Estudar , pesquisar e escolher um dos métodos estatísticos\r\n" + //
+                        "\r\n" + //
+                        "Bonus+\r\n" + //
+                        "\r\n" + //
+                        "-P(A) - calculo simples\r\n" + //
+                        "-Teorema de Bayes. avançado");
     }
 }
