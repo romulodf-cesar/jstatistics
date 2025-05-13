9f76f41ef2f000ce1ecccb5fdfaf5663f174b70d
romulodf-cesar <romulodf.cesar@gmail.com>
2025-05-13 10:45:16 -0300

    Exemplo de Imagem

--- src/AloJardim.java
+++ src/AloJardim.java
@@ -7,29 +7,20 @@ public class AloJardim {
     public static void main(String[] args) {
         try {
             // Obtém o InputStream da imagem dentro do jar (caminho relativo)
-            InputStream inputStream = AloJardim.class.getResourceAsStream("/imagens/joptionpane.png");
-
-            if (inputStream == null) {
-                System.err.println("Erro: A imagem não foi encontrada.");
-                return;
-            }
+            InputStream inputStream = AloJardim.class.getResourceAsStream("/imagens/logo.png");
 
             // Lê a imagem do InputStream
             Image imagem = ImageIO.read(inputStream);
-
-            if (imagem == null) {
-                System.err.println("Erro: A imagem não pôde ser lida.");
-                return;
-            }
+         
 
             // Redimensiona a imagem (opcional)
-            Image imagemReduzida = imagem.getScaledInstance(400, 300, Image.SCALE_SMOOTH);
+            Image imagemReduzida = imagem.getScaledInstance(350, 350, Image.SCALE_SMOOTH);
 
             // Cria o ImageIcon com a imagem redimensionada
             ImageIcon icone = new ImageIcon(imagemReduzida);
 
             // Mostra a imagem no JOptionPane
-            JOptionPane.showMessageDialog(null, "", "Jardim", JOptionPane.PLAIN_MESSAGE, icone);
+            JOptionPane.showMessageDialog(null, "", "JKalango - Framework Educacional",JOptionPane.PLAIN_MESSAGE,icone);
 
         } catch (Exception e) {
             e.printStackTrace();
